4f0de193dbea827a9ba8735e999ffd1bc10ef0c7
Pål-Kristian Hamre <pkhamre@gmail.com>
2012-08-14 11:09:41 +0200

    Removed subscription to old class.

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,7 +3,6 @@ define logstash::service($ensure='running') {
   service { $name:
     ensure    => $ensure,
     subscribe => [
-      Class['logstash::config'],
       Class['logstash::common'],
       Logstash::Initscript[$name],
     ],
