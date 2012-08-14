c7faf5df291a0f7159df005f32cac6c543a68494
Pål-Kristian Hamre <pkhamre@gmail.com>
2012-08-14 11:11:52 +0200

    Create log and run directories.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,8 +5,8 @@
 class logstash::params {
   $bin_dir = '/usr/local/bin'
   $etc_dir = '/etc/logstash'
-  $group   = 'logstash'
-  $log_dir = '/var/log'
-  $pid_dir = '/var/run'
+  $log_dir = '/var/log/logstash'
+  $pid_dir = '/var/run/logstash'
   $user    = 'logstash'
+  $group   = 'logstash'
 }
